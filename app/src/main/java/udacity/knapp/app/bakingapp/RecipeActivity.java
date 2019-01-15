75b7a340d71e502801ac1755ab6591070138a6f1
Daniel Knapp <opendroid@gmail.com>
2019-01-15 07:00:29 -0500

    init recipe list from json

--- app/src/main/java/udacity/knapp/app/bakingapp/RecipeActivity.java
+++ app/src/main/java/udacity/knapp/app/bakingapp/RecipeActivity.java
@@ -2,12 +2,41 @@ package udacity.knapp.app.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+
+import udacity.knapp.app.bakingapp.model.Recipe;
 
 public class RecipeActivity extends AppCompatActivity {
+    private static final String TAG = "RecipeActivity";
+
+    List<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        initRecipeList();
+    }
+
+    private void initRecipeList(){
+        InputStream in = getResources().openRawResource(R.raw.baking);
+        InputStreamReader reader = new InputStreamReader(in);
+        Gson gson = new GsonBuilder().create();
+        Type collectionType = new TypeToken<Collection<Recipe>>(){}.getType();
+        recipeList = gson.fromJson(reader, collectionType);
+        for (Recipe recipe : recipeList) {
+            Log.d(TAG, "initRecipeList: found recipe for " + recipe.getName());
+        }
+
     }
 }
---
+++ app/src/main/java/udacity/knapp/app/bakingapp/model/Ingredient.java
@@ -0,0 +1,31 @@
+package udacity.knapp.app.bakingapp.model;
+
+public class Ingredient {
+    private Double quantity;
+    private String measure;
+    private String ingredient;
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+}
